afd038066cb8b47e63fa6d44e7c18674dc95990f
Arielle Nguyen <ariellephan@gmail.com>
2012-01-28 19:57:29 -0800

    Clean up database and test CRUD

--- kpop.sql
+++ kpop.sql
@@ -1,29 +1,31 @@
--- MySQL dump 10.13  Distrib 5.5.15, for osx10.6 (i386)
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: kpop
--- ------------------------------------------------------
--- Server version	5.5.15
+-- Host: localhost
+-- Generation Time: Jan 29, 2012 at 03:55 AM
+-- Server version: 5.1.44
+-- PHP Version: 5.3.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `kpop`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `kpop`
 --
 
-USE `kpop`;
-DROP TABLE IF EXISTS `kpop`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `kpop` (
+CREATE TABLE IF NOT EXISTS `kpop` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `year` varchar(45) DEFAULT NULL,
@@ -33,27 +35,15 @@ CREATE TABLE `kpop` (
   `description` blob,
   `picture` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=54 ;
 
 --
 -- Dumping data for table `kpop`
 --
 
-LOCK TABLES `kpop` WRITE;
-/*!40000 ALTER TABLE `kpop` DISABLE KEYS */;
-INSERT INTO `kpop` VALUES (1,'SNSD','2007','Genie','Into the New World','Gee','One of top girl groups in Korea','snsd.jpg')
-,(2,'Secret','2010','Secret Times','Shy Boys','Love is Move','5 lovely ladies','secret.jpg');
-/*!40000 ALTER TABLE `kpop` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
-
+INSERT INTO `kpop` (`id`, `name`, `year`, `topalbum`, `debut`, `topsingle`, `description`, `picture`) VALUES
+(1, 'SNSD', '2007', 'Genie', 'Into the New World', 'Gee', 0x4f6e65206f6620746f70206769726c2067726f75707320696e204b6f726561, 'snsd.jpg'),
+(2, 'Secret', '2010', 'Secret Times', 'Shy Boys', NULL, '', 'secret.jpg'),
+(41, '2NE1', '2010', 'I am the best', 'Fire', NULL, '', '2NE1.jpg'),
+(42, 'Wondergirl', '2007', 'The Wonder Years ', 'Tell me', NULL, '', 'Wondergirl.jpg'),
+(53, 'IU', '', 'Good Day', 'Nagging', NULL, '', 'IU.jpg');
